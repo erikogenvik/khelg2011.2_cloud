e8fddaa8c8b8f7bc0b67ace7141b87b2c1233c7d
Jesper <jesper.nilsson@jayway.se>
2011-04-27 19:37:52 +0200

    Added rest module

---
+++ source/server/rest/src/main/java/com/jayway/khelg/rest/EntryResource.java
@@ -0,0 +1,16 @@
+package com.jayway.khelg.rest;
+
+import javax.xml.ws.Response;
+import java.util.Date;
+
+public class EntryResource {
+
+    public Response listEntries() {
+        throw new RuntimeException("not implemented");
+    }
+
+    public Response addEntry() {
+        throw new RuntimeException("not implemented");
+    }
+
+}
---
+++ source/server/rest/src/main/java/com/jayway/khelg/rest/TopicResource.java
@@ -0,0 +1,5 @@
+package com.jayway.khelg.rest;
+
+public class TopicResource {
+
+}
