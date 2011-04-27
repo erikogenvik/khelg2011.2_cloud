3c425fb48639131b331adbd90e626059f9a2feab
Jesper <jesper.nilsson@jayway.se>
2011-04-27 19:48:09 +0200

    added som resource stubs

--- source/server/rest/src/main/java/com/jayway/khelg/rest/ForumResource.java
+++ source/server/rest/src/main/java/com/jayway/khelg/rest/ForumResource.java
@@ -1,5 +1,20 @@
 package com.jayway.khelg.rest;
 
+import javax.xml.ws.Response;
+
+//@Path()
 public class ForumResource {
+
+    public Response listForums() {
+        throw new RuntimeException("not implemented");
+    }
+
+    public Response addForum() {
+        throw new RuntimeException("not implemented");
+    }
+
+    public Response forum() {
+        throw new RuntimeException("not implemented");
+    }
 
 }
